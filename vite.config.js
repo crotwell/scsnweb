f6d9b01a5dbf1f97c3ceb418f40f46f099491382
Philip Crotwell <crotwell@seis.sc.edu>
2025-10-07 15:40:22 -0400

    base url

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  //base: "/scsn/scsnweb/",
+  base: "/scsn/scsnweb/",
   build: {
+    sourcemap: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
