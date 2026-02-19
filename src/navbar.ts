1b6a72dea4e57481a0734bc812df2c4b4dc397c2
Philip Crotwell <crotwell@seis.sc.edu>
2026-02-19 16:48:14 -0500

    better nav bar, separate public from research

--- src/navbar.ts
+++ src/navbar.ts
@@ -26,7 +26,6 @@ let navPublic = `
     <li><a href="${import.meta.env.BASE_URL}historical/index.html">Historical Earthquakes</a></li>
     <li><a href="${import.meta.env.BASE_URL}global/index.html">Global Earthquakes</a></li>
     <li><a href="${import.meta.env.BASE_URL}about/index.html">About</a></li>
-    <li><a href="${import.meta.env.BASE_URL}contact/index.html">Contact</a></li>
   </ul>
 </nav>
 `;
@@ -34,12 +33,8 @@ let navPublic = `
 let navResearch = `
 <nav class="sidebar">
   <ul>
-    <li><a href="${import.meta.env.BASE_URL}scquakes/index.html">Earthquakes</a></li>
-    <li><a href="${import.meta.env.BASE_URL}helicorder/index.html">Today's Seismograms</a></li>
-    <li><a href="${import.meta.env.BASE_URL}historical/index.html">Historical Earthquakes</a></li>
-    <li><a href="${import.meta.env.BASE_URL}global/index.html">Global Earthquakes</a></li>
+    <li><a href="https://eeyore.seis.sc.edu/scsn/status">Status</a></li>
     <li><a href="${import.meta.env.BASE_URL}software/index.html">Software</a></li>
-    <li><a href="${import.meta.env.BASE_URL}about/index.html">About</a></li>
     <li><a href="${import.meta.env.BASE_URL}contact/index.html">Contact</a></li>
   </ul>
 </nav>
@@ -59,6 +54,10 @@ export function createPublicNavigation() {
 export function createResearchNavigation() {
   const navDiv = document.querySelector<HTMLDivElement>('#head')!
   if (navDiv) {
-    navDiv.innerHTML = navHeader + navResearch;
+    navDiv.innerHTML = navHeader;
+  }
+  const innerNav = document.querySelector<HTMLDivElement>('#nav')!
+  if (innerNav) {
+    innerNav.innerHTML = navResearch;
   }
 }
