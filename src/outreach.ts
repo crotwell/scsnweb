853e21f736481006a3501fb0a6c30716e015a06c
Philip Crotwell <crotwell@seis.sc.edu>
2026-02-19 16:46:45 -0500

    public pages as outreach, research page ts

---
+++ src/outreach.ts
@@ -0,0 +1,11 @@
+import './style.css'
+import {createPublicNavigation} from './navbar';
+
+createPublicNavigation();
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+if (false) {
+app.innerHTML = `
+
+`
+}
---
+++ src/research.ts
@@ -0,0 +1,11 @@
+import './style.css'
+import {createResearchNavigation} from './navbar';
+
+createResearchNavigation();
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+if (false) {
+app.innerHTML = `
+
+`
+}
