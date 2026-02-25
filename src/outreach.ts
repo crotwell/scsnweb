0042dad1e68e24b4aa04070559ad50cd20b61f38
Philip Crotwell <crotwell@seis.sc.edu>
2026-02-25 14:29:44 -0500

    move content to src so can interp urls correctly

--- src/outreach.ts
+++ src/outreach.ts
@@ -4,8 +4,22 @@ import {createPublicNavigation} from './navbar';
 createPublicNavigation();
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-if (false) {
+if (true) {
 app.innerHTML = `
 
-`
+<h3>Did an earthquake happen?</h3>
+<p>
+  Did you you feel something and want to know if it was an earthquake?
+  Check the list of <a href="${import.meta.env.BASE_URL}scquakes/index.html">recent earthquakes</a> in South Carolina
+  to see if one has been detected. Note that especially for small
+  earthquakes during non-business times, it may be a few hours before it
+  shows up in the list.
+</p>
+<h3>Where have earthquakes happened in South Carolina?</h3>
+<p>
+  The patterns of <a href="historical">historical seismicity</a>
+  in South Carolina are quite varied.
+
+</p>
+`;
 }
--- src/research.ts
+++ src/research.ts
@@ -4,8 +4,32 @@ import {createResearchNavigation} from './navbar';
 createResearchNavigation();
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-if (false) {
+if (true) {
 app.innerHTML = `
+<h3>Research</h3>
+
+<p>The seismology program at the University of South Carolina is
+  involved in the study of earthquakes and the internal structure of
+  the earth from the crust to the core.
+</p>
+
+<ul class="blurb">
+  <li>
+    <h3>Project A</h3>
+    <a href="https://github.com/crotwell/seisplotjs">project a</a> is a
+      project.
+  </li>
+  <li>
+    <h3>Project B</h3>
+    <a href="https://github.com/crotwell/seisplotjs">project b</a> is a
+      project.
+  </li>
+  <li>
+    <h3>Project C</h3>
+    <a href="https://github.com/crotwell/seisplotjs">project c</a> is a
+      project.
+  </li>
+</ul>
 
 `
 }
