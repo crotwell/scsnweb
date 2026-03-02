ebbeb75fccad67a75efdaeeb3b9a0e8fe30d07ab
Philip Crotwell <crotwell@seis.sc.edu>
2026-03-02 18:13:48 -0500

    dynamic insert flavicon links to head so path is set correctly

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,19 @@
 import {Settings} from 'luxon';
 
 import * as sp from 'seisplotjs';
+export function insertFlavicon() {
+  const flaviconLinks = `
+  <link rel="apple-touch-icon" sizes="180x180" href="${import.meta.env.BASE_URL}favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="${import.meta.env.BASE_URL}favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="${import.meta.env.BASE_URL}favicon/favicon-16x16.png">
+  <link rel="manifest" href="${import.meta.env.BASE_URL}favicon/site.webmanifest">
+  `;
+  document.head.innerHTML += flaviconLinks;
+}
 
 export function init() {
   Settings.throwOnInvalid = true;
+  insertFlavicon();
 
   sp.cssutil.insertCSS(sp.leafletutil.leaflet_css, "spjs_leaflet");
   sp.cssutil.insertCSS(sp.leafletutil.stationMarker_css, "spjs_station");
