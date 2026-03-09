5c461ffe992d2366a0fdd62fc718d949fdba7fe7
Philip Crotwell <crotwell@seis.sc.edu>
2026-03-09 13:34:49 -0400

    move createQuakeTable to map_table

--- src/map_table.ts
+++ src/map_table.ts
@@ -6,8 +6,30 @@ import {
   addQuakesToMap,
   addStationsToMap,
 } from './maplayers';
-import {createQuakeTable} from './util';
 import {Interval} from 'luxon';
+
+
+export const EASTERN_TIMEZONE = new sp.luxon.IANAZone("America/New_York");
+
+
+export function createQuakeTable(quakes: Array<sp.quakeml.Quake>): sp.infotable.QuakeTable {
+    let colDefaultLabels = sp.infotable.QuakeTable.createDefaultColumnLabels();
+    colDefaultLabels.delete(sp.infotable.QUAKE_COLUMN.TIME);
+    //colDefaultLabels.set(sp.infotable.QUAKE_COLUMN.EVENTID, "EventId");
+    let colLabels = new Map();
+    colLabels.set(sp.infotable.QUAKE_COLUMN.LOCALTIME, "Time (Eastern)");
+    for (let k of colDefaultLabels.keys()) {
+      colLabels.set(k, colDefaultLabels.get(k));
+    }
+
+    colLabels.delete(sp.infotable.QUAKE_COLUMN.MAGTYPE);
+
+    let quakeTable = new sp.infotable.QuakeTable([], colLabels);
+    quakeTable.timeZone = EASTERN_TIMEZONE;
+
+    quakeTable.quakeList = quakes;
+    return quakeTable;
+}
 
 
 export function createMapAndTable(divSelector: string = "map",
