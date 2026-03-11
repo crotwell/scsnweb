2246abfad7c3cebd36b4b6183a16a33d49d13d26
Philip Crotwell <crotwell@seis.sc.edu>
2026-03-11 16:24:07 -0400

    base to / for top level deployment on www and eeyore

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/scsn/scsnweb/",
+  //base: "/scsn/scsnweb/",
   build: {
     sourcemap: true,
     rollupOptions: {
