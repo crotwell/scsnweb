6503b4ee491a766ff3762bd582e8d87f0d2c1ce4
Philip Crotwell <crotwell@seis.sc.edu>
2026-06-08 15:07:00 -0400

    fix timezone on seis

--- src/seismogram.ts
+++ src/seismogram.ts
@@ -82,7 +82,7 @@ function displayForQuake(eventid: string) {
       const quakeInfo = document.querySelector('#quakeinfo');
       if (quakeInfo!=null) {
         quakeInfo.textContent =
-        `for ${quake.time.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)} (${latlonF.format(quake.latitude)}, ${latlonF.format(quake.longitude)}) Mag: ${quake.magnitude.mag} Depth: ${depthF.format(quake.depth/1000)}`;
+        `for ${quake.time.setZone(EasternTimeZone).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)} (${latlonF.format(quake.latitude)}, ${latlonF.format(quake.longitude)}) Mag: ${quake.magnitude.mag} Depth: ${depthF.format(quake.depth/1000)}`;
       }
       const filteredStaxml: Array<sp.stationxml.Network> = staxml;//channelsH_Z(staxml);
       const loader = new sp.seismogramloader.SeismogramLoader(filteredStaxml, [quake]);
@@ -124,7 +124,7 @@ function displayForTime(netCode: string, staCode: string, time: DateTime, durati
   const quakeInfo = document.querySelector('#quakeinfo');
   if (quakeInfo!=null) {
     quakeInfo.textContent =
-    `for ${netCode}.${staCode} near ${time.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}`;
+    `for ${netCode}.${staCode} near ${time.setZone(EasternTimeZone).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}`;
   }
   const orgdisp = document.querySelector(sp.organizeddisplay.ORG_DISPLAY) as sp.organizeddisplay.OrganizedDisplay;
   orgdisp.setAttribute(sp.leafletutil.FIT_BOUNDS, "false");
